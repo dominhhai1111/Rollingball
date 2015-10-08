e0faef9349b30e34c56141937aa592c8bf15bd49
Do Minh Hai <dominhhai@MacBook-Air.local>
2015-10-08 21:27:18 +0700

    first commit

--- Rollingball/ViewController.m
+++ Rollingball/ViewController.m
@@ -13,15 +13,52 @@
 @end
 
 @implementation ViewController
-
+{
+    UIImageView *ball;
+    NSTimer *timer;
+    CGFloat angle;
+    CGFloat ballRadius;
+     bool daoBong  ;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self addBall];
+    angle=0.0;
+    timer = [NSTimer scheduledTimerWithTimeInterval:0.05 target:self selector:@selector(rollBall) userInfo:nil repeats:true];
+    }
+-(void) addBall
+{
+    CGSize mainViewSize = self.view.bounds.size;
+    
+    ball= [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Soccer.png"]];
+    ballRadius = 48.0;
+    ball.center = CGPointMake(ballRadius, mainViewSize.height * 0.5);
+    [self.view addSubview:ball];
+    
 }
+-(void) rollBall
+{
+   
+    CGSize mainViewSize = self.view.bounds.size;
+    if (ball.center.x >= mainViewSize.width - ballRadius) {
+        daoBong = NO;
+    }
+    if (ball.center.x <= ballRadius) {
+        daoBong = YES;
+    }
+    if (daoBong == YES) {
+        CGFloat deltaAngle = 0.1;
+        angle += deltaAngle;
+        ball.transform = CGAffineTransformMakeRotation(angle);
+        ball.center = CGPointMake(ball.center.x + ballRadius*deltaAngle, ball.center.y);
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    }else
+    {
+        CGFloat deltaAngle = 0.1;
+        angle -= deltaAngle;
+        ball.transform = CGAffineTransformMakeRotation(angle);
+        ball.center = CGPointMake(ball.center.x - ballRadius*deltaAngle, ball.center.y);
+    }
 }
 
 @end
